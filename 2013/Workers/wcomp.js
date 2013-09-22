3dea4a18175bdb08c2697b075263fe3d614e3092
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-09-22 12:43:11 +0200

    Workers

---
+++ 2013/Workers/wcomp.js
@@ -0,0 +1,30 @@
+var has_to_finish = false;
+
+// Handler for the messages sent to the worker
+postMessage({type:'alert',data:'Worker created'});
+
+onmessage = function(m) {
+	if(m.data.type == 'start')  {
+		has_to_finish = false;
+		compute();
+	}
+}
+
+function compute() {
+	var num_times = 0;
+	do {
+		for(var i = 0; i < 1000000; i++) {
+			var a = 1234;
+		}
+		num_times++;
+		if(num_times <= 100)  {
+			postMessage({type:'progress',data:num_times});
+		}
+
+		if(num_times == 100) {
+			has_to_finish = true;
+		}
+	} while(!has_to_finish)
+
+	postMessage({type:'alert',data:'Computation finished'});
+}
