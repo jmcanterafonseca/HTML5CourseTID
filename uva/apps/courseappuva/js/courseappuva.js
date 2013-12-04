53c58a5ba2966db262351632dcf0700e93ed17e3
Jose M. Cantera <jmcanterafonseca@gmail.com>
2013-12-04 17:35:14 +0100

    added

--- uva/apps/courseappuva/js/courseappuva.js
+++ uva/apps/courseappuva/js/courseappuva.js
@@ -1,13 +1,39 @@
-var MyContactsModule= (function() {
-  function list() {
-    var nodeList = document.getElementById('contacts-list');
+'use strict';
 
+var MyContactsModule= (function() {
+  var detailSection, listSection;
+
+  function start() {
+    init();
+    list();
+  }
+
+  function init() {
+    listSection = document.getElementById('contacts-list');
+    detailSection = document.getElementById('detail');
+
+    listSection.addEventListener('click', showDetail);
+
+    document.getElementById('back').addEventListener('click', goBack);
+  }
+
+  function goBack() {
+    detailSection.addEventListener('transitionend', function tend() {
+      detailSection.removeEventListener('transitionend', tend);
+      detailSection.classList.remove('right-to-left');
+      detailSection.classList.remove('back-to-right');
+    });
+
+    detailSection.classList.add('back-to-right');
+  }
+
+  function list() {
     var req = navigator.mozContacts.find({});
     req.onsuccess = function() {
       var contacts = req.result;
       contacts.forEach(function(aContact) {
         var contactNode = buildContactNode(aContact);
-        nodeList.appendChild(contactNode);
+        listSection.appendChild(contactNode);
       });
     }
 
@@ -16,8 +42,16 @@ var MyContactsModule= (function() {
     }
   }
 
+  function showDetail(e) {
+    var target = e.target;
+    detailSection.classList.add('right-to-left');
+    var name = target.parentNode.dataset.name;
+    detailSection.querySelector('h1').textContent = name;
+  }
+
   function buildContactNode(aContact) {
     var liNode = document.createElement('li');
+    liNode.dataset.name = aContact.name[0];
     var aNode = document.createElement('a');
 
     liNode.appendChild(aNode);
@@ -37,8 +71,6 @@ var MyContactsModule= (function() {
   }
 
   return {
-    'list': list
+    'start': start
   }
 })();
-
-MyContactsModule.list();
---
+++ uva/apps/courseappuva/js/courseappuva_init.js
@@ -0,0 +1,1 @@
+MyContactsModule.start();
