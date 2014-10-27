eb779326415207941b212433ba363a3b7dcc128a
Jose M. Cantera <jmcanterafonseca@gmail.com>
2014-10-27 17:06:57 +0100

    done

--- beyond_html5/serviceworkers/worker.js
+++ beyond_html5/serviceworkers/worker.js
@@ -3,19 +3,20 @@
 console.log('SW started');
 
 this.addEventListener('install', function(event) {
+  /*
   event.waitUntil(caches.create('v1').then(function(cache) {
     return cache.add('star-wars-logo.jpg');
-  }));
+  })); */
 
   console.log('Service Worker have been installed');
 });
 
 this.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!");
-
+/*
   var cachedResponse = caches.match(event.request).catch(function() {
     return new Response("Hello world!");
   });
-
+*/
   event.respondWith(new Response("Hello world!"));
 });
