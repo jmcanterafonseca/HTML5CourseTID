4211669c260da287a2306f3a6eb6715afa520f37
Jose M. Cantera <jmcanterafonseca@gmail.com>
2014-10-28 11:20:41 +0100

    done

--- beyond_html5/serviceworkers/worker.js
+++ beyond_html5/serviceworkers/worker.js
@@ -5,9 +5,9 @@ importScripts('vendor/serviceworker-cache-polyfill.js');
 console.log('SW started');
 
 this.addEventListener('install', function(event) {
-  event.waitUntil(cachesPolyfill.open('myapp-static-v1').then(function(cache) {
+  event.waitUntil(cachesPolyfill.open('myapp-static-v2').then(function(cache) {
     console.log('Adding to the cache');
-    return cache.add('myapp/star-wars-logo.jpg');
+    return cache.add('images/star-wars-logo.jpg');
   }));
   console.log('Service Worker have been installed');
 });
@@ -19,7 +19,14 @@ this.addEventListener('activate', function(event) {
 this.addEventListener('fetch', function(event) {
   console.log("Caught a fetch!");
 
-  event.respondWith(cachesPolyfill.match(event.request).then(function(response) {
-    return response || fetch(event.request);
-  }));
+  var request = event.request;
+
+  if (request.url.indexOf('star-wars-logo.jpg') !== -1) {
+    event.respondWith(cachesPolyfill.match(
+                        'images/star-wars-logo.jpg').then(function(response) {
+      return response;
+    }));
+  }
+
+  event.respondWith(new Response('Hello World'))
 });
