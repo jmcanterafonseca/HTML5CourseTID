9aaa13e531d86dc40e89a0f5a31e9ec5b1d328b5
Jose M. Cantera <jmcanterafonseca@gmail.com>
2014-12-29 13:02:17 +0100

    done

--- beyond_html5/promise/rejections.js
+++ beyond_html5/promise/rejections.js
@@ -32,7 +32,17 @@ function doNoRejectFunction() {
   });
 }
 
-function doCatchFunction() {
+function doCatchFunction1() {
+  clear();
+
+  rejectMe().then(() => {
+    error('It should not be called');
+  }).catch((e) => {
+      error('Catched: ', e.name);
+  });
+}
+
+function doCatchFunction2() {
   clear();
 
   resolveMe().then(() => {
