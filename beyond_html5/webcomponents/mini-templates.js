667917fd99e2e490fc549adf8d11a2b2022f444a
Jose M. Cantera <jmcanterafonseca@gmail.com>
2015-01-14 16:23:20 +0100

    done

---
+++ beyond_html5/webcomponents/mini-templates.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function templateReplace(data) {
+  return function(text, property) {
+    var out = data[property];
+    if (typeof out === 'undefined') {
+      out = text;
+    }
+    return out;
+  }
+}
+
+var Templates = {
+  append: function(container, data) {
+    var template =  container.querySelector('template');
+    // This is a document fragment
+    var templateFragment = template.content;
+
+    // We import the root element and then clone
+    var newElem = document.importNode(
+                templateFragment.firstElementChild, true);
+    var inner = newElem.innerHTML;
+
+    var pattern = /#(\w+[\w.]*)#/g;
+    // Replace function
+    var replaceFunction = templateReplace(data);
+    var ninner = inner.replace(pattern, replaceFunction);
+
+    newElem.innerHTML = ninner;
+
+    container.appendChild(newElem);
+  }
+}
