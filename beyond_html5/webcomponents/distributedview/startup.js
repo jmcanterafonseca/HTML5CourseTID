c35ffa33f41bdb45978db4d498633ec760e9e2ea
Jose M. Cantera <jmcanterafonseca@gmail.com>
2015-01-21 17:16:09 +0100

    done

--- beyond_html5/webcomponents/distributedview/startup.js
+++ beyond_html5/webcomponents/distributedview/startup.js
@@ -1,7 +1,19 @@
 'use strict';
 
 function loadView1() {
-  document.getElementById('notes-list-view').load();
+  document.getElementById('notes-list-view').load().then((node) => {
+    var dataList = [
+      {
+        id: 'l1',
+        text: 'Text of element 1'
+      },
+      {
+        id: 'l2',
+        text: 'Text of element 2'
+      }
+    ];
+    node.querySelector('x-list').ref = dataList;
+  });
 }
 
 function loadView2() {
