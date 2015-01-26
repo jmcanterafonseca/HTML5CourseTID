9c0029f0dce0b191e8b4630e3097935e1ab0e7b8
Jose M. Cantera <jmcanterafonseca@gmail.com>
2015-01-26 15:31:37 +0100

    done

--- beyond_html5/serviceworkers/service_worker.js
+++ beyond_html5/serviceworkers/service_worker.js
@@ -14,10 +14,14 @@ function registerSW() {
       scope: 'myapp/'
     }).then(function(registration) {
         showMsg('SW registered under scope: ' + registration.scope);
+        if (registration.installing) {
+          registration.installing.postMessage('hello!!');
+        }
     }).catch(function(err) {
         showMsg('Registration failed: ' + err.name);
     });
 
+    // This not called yet by Google Chrome's implementation
     navigator.serviceWorker.ready.then(function() {
       showMsg('SW is now in the active state');
     });
